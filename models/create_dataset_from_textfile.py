aee3412c3544de288b8e7a2e27b7c0d10c358d63
Krišjānis <65333249+chrisAS12@users.noreply.github.com>
2022-01-08 11:30:13 +0200

    Trying to fix model not training

--- models/create_dataset_from_textfile.py
+++ models/create_dataset_from_textfile.py
@@ -1,14 +1,18 @@
-import torch
-def create_encodings(text):
-    splitted = text.split('\n')
-    labels = torch.tensor([x for x in splitted.input_ids])
-    mask = torch.tensor([x for x in batch.attention_mask])  
-    input_ids = labels.detach().clone()
-    rand = torch.rand(input_ids.shape)
-    mask_arr = (rand < .15) * (input_ids != 0) * (input_ids != 1) * (input_ids != 2)
-    for i in range(input_ids.shape[0]):
-        selection = torch.flatten(mask_arr[i].nonzero()).tolist()
-        input_ids[i, selection] = 3 
-    encodings = {'input_ids': input_ids, 'attention_mask': mask, 'labels': labels}
-    return encodings
+import spacy
+import pandas as pd 
+from torchtext.legacy.data import Field, BucketIterator, TabularDataset
+from sklearn.model_selection import train_test_split
 
+text = open('data/sentences_seperated_by_lines_format.txt', encoding='utf8').read().split('\n')
+
+raw_data = {'Text':[line for line in text[1:2000]]}
+
+df = pd.DataFrame(raw_data, columns=['Text'])
+
+train, test = train_test_split(df, test_size=0.2)
+
+train.to_json('train.json', orient='records', lines=True)
+
+
+#def encode_with_truncation(examples):
+    #return tokenizer_default(examples["text"], truncation=True, padding="max_length", max_length=1024, return_special_tokens_mask=True)
--- models/custom_modified_bert_training_new.py
+++ models/custom_modified_bert_training_new.py
@@ -1,50 +1,57 @@
 import os
-os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+#os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
 from tokenizers import Tokenizer
 import torch
 from transformers import BertTokenizerFast, BertConfig, BertForPreTraining
-from transformers import TextDatasetForNextSentencePrediction, DataCollatorForLanguageModeling, Trainer, TrainingArguments
+from transformers import LineByLineTextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
 from datasets import load_dataset
 import json
 
 
 model_path = "tokenizer_30522_default_vocab_truncation_1024"
 
-tokenizer_default = BertTokenizerFast.from_pretrained(model_path, do_lower_case=True)
+tokenizer_default = BertTokenizerFast.from_pretrained(
+    model_path, do_lower_case=True)
 
 config = BertConfig()
 model = BertForPreTraining(config)
 
 #dataset = load_dataset("cc_news", split="train")
 
-def encode_with_truncation(examples):
-  return tokenizer_default(examples["text"], truncation=True, padding="max_length", max_length=1024, return_special_tokens_mask=True)
+dataset = LineByLineTextDataset(
+    tokenizer=tokenizer_default,
+    file_path="data/sentences_seperated_by_lines_format.txt",
+    block_size=1024,
+)
 
 
-#train_dataset = dataset["train"].map(encode_with_truncation, batched=True)  
+
+#train_dataset = dataset["train"].map(encode_with_truncation, batched=True)
 #train_dataset.set_format(type="torch", columns=["input_ids", "attention_mask"])
 
+print(dataset)
 
 print("data_colator")
 data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer_default,
+    pad_to_multiple_of=2,
     mlm=True,
     mlm_probability=0.2
 )
 
 print("training_args")
 training_args = TrainingArguments(
-    output_dir= "/bert_output/",
+    output_dir="/bert_output/",
     overwrite_output_dir=True,
     num_train_epochs=3,
-    per_device_train_batch_size =4,
-    per_device_eval_batch_size=16,   
-    #save_total_limit=2,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=16,
+    # save_total_limit=2,
     learning_rate=2e-5,
-    evaluation_strategy="steps", 
-    logging_steps=500,            
+    evaluation_strategy="steps",
+    logging_steps=500,
     save_steps=500,
-    #prediction_loss_only=True,
+    # prediction_loss_only=True,
     logging_dir='logs',
 )
 
@@ -54,8 +61,8 @@ trainer = Trainer(
     args=training_args,
     data_collator=data_collator,
     train_dataset=dataset,
-    #gpus=8,
-    #strategy="ddp"
+    # gpus=8,
+    # strategy="ddp"
 )
 
 print("training")
